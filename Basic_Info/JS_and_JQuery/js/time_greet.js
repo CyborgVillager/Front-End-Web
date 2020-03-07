45ba9793b6b10c88c0bd44d74dcc282b80624900
CyborgVillager <almawijonathan@gmail.com>
2020-03-06 22:53:11 -0600

    v29 Greeting JS
     Greeting JS: basic js greeting @ new folder js_and_jquery. js folder hold the js file time_greet.js. time_greet.html
                        has html info & style0.css has the css info. basic summary of using js to be used for a web-page.
                        made basic additions to the file to show some visual examples of combining, html->css and js together.

---
+++ Basic_Info/JS_and_JQuery/js/time_greet.js
@@ -0,0 +1,22 @@
+/* Variables */
+var today = new Date();
+var hourNow = today.getHours()
+var greeting;
+
+/* Military Time */
+/* http://militarytimechart.com/ */
+/* if the hour is greater than 6pm -> 18 its evening */
+if (hourNow > 18) {
+    greeting = 'Good evening!';
+/* if the hour is greater than 12pm -> 12 its afternoon */
+} else if (hourNow > 12){
+    greeting = 'Good afternoon!';
+/* if the hour is greater than 12am -> 0 its morning */
+} else if(hourNow > 0){
+    greeting = 'Good morning!';
+/* if the info is not on here, just say Welcome */
+} else{
+    greeting = 'Welcome!'
+}
+
+document.write('<h3>' + greeting + '</h3>');
