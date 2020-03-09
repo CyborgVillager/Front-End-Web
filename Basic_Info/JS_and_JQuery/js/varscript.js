d88f4fb3db9cf89e107cf7f6e581e5e9a6ed6131
CyborgVillager <almawijonathan@gmail.com>
2020-03-09 08:34:00 -0500

    v32 MSG.html & let
    MSG.html & let: using let to declare a var/value. Besides that basic msg.html just tells the user hello & random
                            info alerts.

---
+++ Basic_Info/JS_and_JQuery/js/varscript.js
@@ -0,0 +1,16 @@
+/* to make a var */
+let msg4user;
+
+message = 'Hello User!'
+
+/* basically a print statement */
+alert(message)
+
+/* multi variables */
+let username='Jonathan';
+let age=555;
+let message4user2='^_^';
+
+alert(username)
+alert('is over ' + age +' years old')
+alert(message4user2)
