475702f1bb1f3d87dd484b2165f8ccd57f3a9dac
CyborgVillager <almawijonathan@gmail.com>
2020-03-10 09:04:08 -0500

    v41 Custom Logo
    Custom Logo: custom logo / modified App.js, will be making this into a todo list format.
    			tutorial for a quick react -> https://www.youtube.com/watch?v=sBws8MSXN7A

--- React/react_project0/src/App.js
+++ React/react_project0/src/App.js
@@ -7,8 +7,11 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+        <h1>Hello!</h1>
+
         <p>
           Edit <code>src/App.js</code> and save to reload.
+          <i> ^_*</i>
         </p>
         <a
           className="App-link"
