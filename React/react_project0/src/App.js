35706d1fa4fb87704a403e6952613b0667ed20e2
CyborgVillager <almawijonathan@gmail.com>
2020-03-10 09:35:18 -0500

    ToDos information : new file Todos.js, to make a 'todo' list, modified the file for the list
    ToDos information : new file Todos.js, to make a 'todo' list, modified the file for the list

--- React/react_project0/src/App.js
+++ React/react_project0/src/App.js
@@ -1,29 +1,27 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Todos from "./components/Todos";
 
 function App() {
+  /* JSX start */
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <h1>Hello!</h1>
+        <h1>Application</h1>
 
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          One step @ a time.
           <i> ^_*</i>
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+
       </header>
+    <Todos/>
+
     </div>
   );
+    /* JSX end */
 }
 
 export default App;
---
+++ React/react_project0/src/components/Todos.js
@@ -0,0 +1,20 @@
+/* for js Cap first letter, python lower case
+ First letter for begin of filename
+ */
+
+import React from 'react';
+
+
+function Todos() {
+  /* JSX start */
+  return (
+      <div className={'ToDos'}>
+        <h1>Todos</h1>
+
+      </div>
+
+  );
+    /* JSX end */
+}
+
+export default Todos;
